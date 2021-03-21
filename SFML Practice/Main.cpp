ee3a92dfc5849d443ffce90d81a380d3c0d57d02
Rhamsez Thevenin <rhamzthev@gmail.com>
2021-03-21 19:33:12 -0400

    Playin around with this library a lil bit.

--- SFML Practice/Main.cpp
+++ SFML Practice/Main.cpp
@@ -1,9 +1,55 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+using namespace sf;
+using namespace std;
+
 int main() {
     
-	std::cout << "Hello World" << std::endl;
+	//This makes a window
+	RenderWindow window(VideoMode(500, 500), "Test Window", Style::Resize | Style::Close);
+
+
+	//This makes a while loop to keep open.
+	while (window.isOpen()) 
+	{
+		Event e;
+
+		while (window.pollEvent(e)) 
+		{
+			
+			switch (e.type) 
+			{
+			case Event::Closed:
+				//Closes window
+				cout << "Goodbye!" << endl;
+				window.close();
+				break;
+			case Event::Resized:
+				//Gives us the size of resized window
+				cout << "New window width: " << e.size.width << " New window height: " << e.size.height << endl;
+				break;
+			case Event::TextEntered:
+				//returns entered text into the terminal
+				if (e.text.unicode < 128) 
+				{
+					printf("%c", e.text.unicode);
+				}
+			}
+
+			
+			
+			if (e.type == e.Closed) 
+			{
+				window.close();
+			}
+
+			if (e.type == e.Resized) 
+			{
+
+			}
+		}
+	}
 
 	return 0;
 }
