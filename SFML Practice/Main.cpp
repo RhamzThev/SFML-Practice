b6477780783b7e4510b64f9035bc559199cb8977
RhamzThev <rhamzthev@gmail.com>
2021-03-22 21:17:29 -0400

    texturerects so cool wow

--- SFML Practice/Main.cpp
+++ SFML Practice/Main.cpp
@@ -7,14 +7,24 @@ using namespace std;
 int main() {
     
 	//This makes a window
-	RenderWindow window(VideoMode(500, 500), "Test Window", Style::Resize | Style::Close);
+	int size = 500;
+	RenderWindow window(VideoMode(size, size), "Test Window", Style::Resize | Style::Close);
 
-	CircleShape rectangle(10.0f);
+	float rectangleSize = 500.0f;
+	RectangleShape rectangle(Vector2f(rectangleSize, rectangleSize));
+	float center = ((float)size / 2.0f) - (rectangleSize / 2.0f);
+	rectangle.setPosition(center, center);
 	//rectangle.setFillColor(Color::Red);
 
 	Texture texture;
-	texture.loadFromFile("C:/Users/coolr/Visual Studio Code/gamedev/SFML Practice/SFML Practice/Textures/micro_logo.png");
+	texture.loadFromFile("C:/Users/coolr/Visual Studio Code/gamedev/SFML Practice/SFML Practice/Textures/checkers.png");
 	rectangle.setTexture(&texture);
+
+	Vector2u textureSize = texture.getSize();
+	textureSize.x /= 5;
+	textureSize.y /= 5;
+
+	rectangle.setTextureRect(IntRect(textureSize.x * 2, textureSize.y * 4, textureSize.x, textureSize.y));
 
 	//This makes a while loop to keep open.
 	while (window.isOpen()) 
@@ -31,20 +41,20 @@ int main() {
 				cout << "Goodbye!" << endl;
 				window.close();
 				break;
-			case Event::Resized:
-				//Gives us the size of resized window
-				cout << "New window width: " << e.size.width << " New window height: " << e.size.height << endl;
-				break;
-			case Event::TextEntered:
-				//returns entered text into the terminal
-				if (e.text.unicode < 128) 
-				{
-					printf("%c", e.text.unicode);
-				}
+			//case Event::Resized:
+			//	//Gives us the size of resized window
+			//	cout << "New window width: " << e.size.width << " New window height: " << e.size.height << endl;
+			//	break;
+			//case Event::TextEntered:
+			//	//returns entered text into the terminal
+			//	if (e.text.unicode < 128) 
+			//	{
+			//		printf("%c", e.text.unicode);
+			//	}
 			}
 		}
 
-		if (Keyboard::isKeyPressed(Keyboard::Key::W) || Keyboard::isKeyPressed(Keyboard::Key::Up))
+		/*if (Keyboard::isKeyPressed(Keyboard::Key::W) || Keyboard::isKeyPressed(Keyboard::Key::Up))
 		{
 			rectangle.move(0.0f, -0.1f);
 		}
@@ -68,7 +78,7 @@ int main() {
 		{
 			Vector2i mouse = Mouse::getPosition(window);
 			rectangle.setPosition((float)(mouse.x), (float)(mouse.y));
-		}
+		}*/
 
 		window.clear();
 		window.draw(rectangle);
